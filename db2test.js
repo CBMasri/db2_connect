93790ae718507589a5239c56e827777039f08609
Carl Masri <carl.masri@gmail.com>
2017-05-08 11:38:21 -0700

    Add stored procedure capabilities

--- db2test.js
+++ db2test.js
@@ -6,6 +6,7 @@
 try {
 	var ibmdb = require('ibm_db');
 	var minimist  = require('minimist');
+	var assert = require('assert');
 } catch (e) {
 	console.log("error: one or more modules are not installed");
 	console.log(e);
@@ -28,59 +29,101 @@ for (var key in args) {
 		if (key !== "_" && validOptions.indexOf(key) === -1) {
 			console.log("error: invalid argument specified: " + key);
 			console.log("Usage: node db2test.js -d <database> -u <user> -w <password> -h <host> -p <port>");
-			return (-1);
+			process.exit(-1);
 		}
 	}
 }
 
-console.log("Attempting to connect to DB2 database...");
+var connectionString = "DRIVER={DB2};DATABASE=" + args.d + ";UID=" + args.u + ";PWD=" + args.w + ";HOSTNAME=" + args.h + ";port=" + args.p;
+
+console.log("\nAttempting to connect to DB2 database...");
 
 /*
     Connect to the database server
-    param 1: The DSN string which has the details of database name to connect to, user id, password, hostname, portnumber 
-    param 2: The Callback function to execute when connection attempt to the specified database is completed
+      param 1: The DSN string which has the details of database name to connect to, user id, password, hostname, portnumber 
+      param 2: The Callback function to execute when connection attempt to the specified database is completed
 */
-ibmdb.open("DRIVER={DB2};DATABASE=" + args.d + ";UID=" + args.u + ";PWD=" + args.w + ";HOSTNAME=" + args.h + ";port=" + args.p, function(err, conn)
+ibmdb.open(connectionString, function(err, conn)
 {
-        if(err) {
-		/*
-		    On error in connection, log the error message on console 
-		*/
-          	console.error("error: ", err.message);
-        } else {
+        if (err) {
+          	console.error("error: connection to database failed: ", err.message);
+          	process.exit(-1);
+        }
+
+       	console.log("Connection successful.\n");
+
+       	/*
+			Set up a stored procedure with parameters
+       	*/
+       	var schema = "DB2INST1";
+		var proc1 = "create or replace procedure " + schema + ".proc1 " +
+       				"(IN v1 INTEGER, OUT v2 INTEGER, INOUT v3 VARCHAR(20)) " +
+       				"BEGIN set v2 = v1 + 1; set v3 = 'verygood'; END";
+		var query = "call " + schema + ".proc1(?, ?, ?)";
 
 		/*
-			On successful connection issue the SQL query by calling the query() function on Database
-			param 1: The SQL query to be issued
-			param 2: The callback function to execute when the database server responds
+			Create stored procedure in DB2
+			  querySync: synchronously issue a SQL query to the database that is currently open
+			    param1: sqlQuery: the SQL query to be executed
+			    param2: bindingParameters (optional): an array of values that will be bound to any '?' characters in sqlQuery
 		*/
-		conn.query("SELECT * FROM <TABLE>", function(err, records, moreResultSets) {
+		conn.querySync(proc1);
 
-            console.log("<ATTR1> \t <ATTR2> \t <ATTR3> \t <ATTR4> \t <ATTR5>");
-			console.log("------- \t ------- \t ------- \t ------- \t -------");
+       	/*
+       		bindingParameters
+       		  paramType: type of the parameter (INPUT, OUTPUT, INOUT, FILE)
+       		  DataType: data type (aka column type) of the parameter on the server. Default is CHAR.
+       		  Data: actual data value for the parameter
+       		  Length: (for CHAR datatype) specifies the 
+       	*/
+       	var param1 = {ParamType:"INPUT", DataType:"INTEGER", Data:0};
+	    var param2 = {ParamType:"OUTPUT", DataType:"INTEGER", Data:0};
+		var param3 = {ParamType:"INOUT", DataType:"CHAR", Data:"test", Length:30};
+
+		/*
+			Now that the stored procedure has been created we can call it
+			  Each '?' in .proc1(?, ?, ?) will be replaced by the defined param
+			  Driver will throw an error if the number of params don't match
+		*/
+		var result = conn.querySync(query, [param1, param2, param3]);
+		
+		/*
+			Do something with the result
+		*/
+		console.log(result);
+
+		/*
+			On successful connection issue the SQL query by calling the async query() function on the database
+			  param 1: The SQL query to be issued
+			  param 2: The callback function to execute when the database server responds
+		*/
+		conn.query("SELECT * FROM TSYSTEM", function(err, records, moreResultSets) {
+
+            console.log("INTSYSTEMID \t CHRHOSTNAME \t\t\t CHRTYPE \t INTHTTPPORT \t CHRHTTPINTERFACE");
+			console.log("----------- \t ----------- \t\t\t ------- \t ----------- \t ----------------");
 
 			/*
-				Loop through the rows returned from the select query and print.
-				Requires knowledge of at least some of the target attributes.
+				Loop through the rows returned from the select query and print
+				  Requires knowledge of at least some of the target attributes
 			*/
             for (var i = 0; i < records.length; i++)
 			{
-				console.log(records[i].ATTR1, "\t",
-							records[i].ATTR2, "\t",
-							records[i].ATTR3, "\t",
-							records[i].ATTR4, "\t",
-							records[i].ATTR5
+				console.log(records[i].INTSYSTEMID, "\t\t",
+							records[i].CHRHOSTNAME,
+							records[i].CHRTYPE, "\t\t",
+							records[i].INTHTTPPORT, "\t\t",
+							records[i].CHRHTTPINTERFACE
 				);
 			}
-			console.log("---------------------------------------------------------");
+			console.log("-------------------------------------------------------------------------------------------------");
 
 			/*
 				Close the connection to the database.
-				param 1: The callback function to execute on completion of close function.
+				  param 1: The callback function to execute on completion of close function.
 			*/
+
 			conn.close(function(){
-				console.log("Connection Closed.");
+				console.log("\nConnection Closed.\n");
 			});
 		});
-	}
 });
